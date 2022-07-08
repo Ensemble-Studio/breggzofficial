03884dfcae6f93090aced4686612d83695cac74b
Ensemble-Studio <69461395+Ensemble-Studio@users.noreply.github.com>
2022-07-08 18:22:33 +0200

    cookie mobile

--- web/src/components/Cookie.js
+++ web/src/components/Cookie.js
@@ -5,23 +5,21 @@ const Cookie = () => {
   if (show)
     return (
       <div
-        className='flex justify-center items-center relative overflow-hidden gap-5 px-10 py-5 rounded-[3px] bg-white'
+        className='flex md:flex-row flex-col justify-center items-center  overflow-hidden gap-5 px-5 md:px-10 py-5 rounded-[3px] bg-white absolute md:bottom-8 bottom-[8%] w-4/5 md:w-1/2 left-[10%] md:left-[25%] z-40'
         style={{ boxShadow: "0px 4px 10px 0 rgba(0,0,0,0.1)" }}
       >
-        <p className='flex-grow-0 flex-shrink-0 text-base text-left text-black'>
-          Cookies
-        </p>
-        <p className='flex-grow-0 flex-shrink-0 opacity-60 text-sm font-medium text-left text-black'>
+        <p className='text-xl md:text-base text-left text-black'>Cookies</p>
+        <p className='opacity-60 text-sm font-medium md:text-left text-black text-center'>
           By using this website, you automatically accept we use cookies.
         </p>
-        <div className='flex justify-center items-center flex-grow-0 flex-shrink-0 w-[124px] relative gap-2.5 p-2 rounded-[5px] bg-[#235251]/90 border-[0.5px] border-[#12151d]/90'>
-          <button
-            onClick={() => setShow(false)}
-            className='flex-grow-0 flex-shrink-0 opacity-90 text-sm text-left uppercase text-white'
-          >
+        <button
+          className='flex justify-center items-center  w-[124px] relative gap-2.5 p-2 rounded-[5px] bg-[#235251]/90 border-[0.5px] border-[#12151d]/90'
+          onClick={() => setShow(false)}
+        >
+          <a className='flex-grow-0 flex-shrink-0 opacity-90 text-sm text-left uppercase text-white'>
             accept
-          </button>
-        </div>
+          </a>
+        </button>
       </div>
     );
 };
